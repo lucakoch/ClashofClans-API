204a3bafba4dcac27dd8c79c1317a987450fcc6b
BuildTools <unconfigured@null.spigotmc.org>
2022-10-26 21:06:40 +0200

    main.py update

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='clashofclans-api',
+    version='0.0.2',
+    description='This is an easy to use API of the game Clash of Clans',
+    # long_description="",
+    author='Luca Sebastian Koch',
+    author_email='luca.sebastian.koch@gmail.com',
+    # maintainer="",
+    # maintainer_email="",
+    url='https://github.com/Acul747/Clash-of-Clans-API',
+    # download_url="",
+    license='MIT',
+    packages=[''],
+    install_requires=[
+        '',
+    ],
+    zip_safe=False)
